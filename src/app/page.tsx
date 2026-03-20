e294860fa48c3552de9cf18a132c8f9176d028a5
skidder67 <youremail@gmail.com>
2026-03-19 20:12:09 -0400

    update profile pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
           src="https://i.imgur.com/L2UWkYf.png"
           className="w-24 h-24 rounded-full"
         />
-        <h1 className="text-2xl font-bold">@skidder67</h1>
-        <p className="text-gray-400">my bio goes here</p>
+        <h1 className="text-2xl font-bold">@$HIESTY</h1>
+        <p className="text-gray-400">Game Protector, Modder, Coder(C++, C#, LUA)</p>
 
         <div className="flex flex-col gap-3 w-full mt-4">
           <a href="#" className="bg-white/10 hover:bg-white/20 rounded-xl px-4 py-3 text-center transition">
